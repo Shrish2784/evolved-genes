2a76237f50bd75717dfcaf1268c6b98569ef0274
Shrish Asthana <shrishasthana@outlook.com>
2020-04-01 20:33:39 +0530

    heroku serve

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route} from "react-router-dom";
-import {Switch} from "react-bootstrap";
+import {HashRouter, Link, Route} from "react-router-dom";
 import Writer from "./writer/app/components/Writer";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
@@ -10,24 +9,26 @@ import Rocket from "./rockets/app/components/Rocket";
 function App() {
   return (
     <div>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="/">{Config.projectName}</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            {Object.entries(Config.apps).map(([key, val]) =>
-              <Nav.Link key={key} href={val.appUrl}>{val.appName}</Nav.Link>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <HashRouter>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="/">{Config.projectName}</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              {Object.entries(Config.apps).map(([key, val]) =>
+                <Nav.Item className="nav-link">
+                  <Link className="evolve-link" to={val.appUrl}>{val.appName}</Link>
+                </Nav.Item>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
 
-      <BrowserRouter>
-        <Switch>
+        <div>
           <Route path={Config.apps.Writer.appUrl} component={Writer}/>
           <Route path={Config.apps.Rockets.appUrl} component={Rocket}/>
-        </Switch>
-      </BrowserRouter>
+        </div>
+      </HashRouter>
     </div>
   );
 }
